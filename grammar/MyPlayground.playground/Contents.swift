69e9b431cb104f63c71e7b3790c9fddde70c83c9
rlfah303 <rlarlfah303@gmail.com>
2021-12-01 19:45:32 +0900

    add playground

---
+++ grammar/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Swift
+
+let age: Int = 30
+
+print("안녕하세요 저는 \(age)살 입니다")
+
+class Person{
+    let name: String = "gilmo"
+    let age: Int = 10
+}
+
+let gilmo: Person = Person()
+
+print(gilmo )
+
+print("\n------------------------")
+
+dump(gilmo)
+
+
